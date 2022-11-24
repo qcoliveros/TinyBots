be9599f8c961f5ac05292373c9d787a0017518ad
qcoliveros@gmail.com <SSuCM0b5Aj44>
2022-11-24 16:28:33 +0800

    Added scheduler to run the mail to telegram forwarder every 3 seconds.

--- MailToTelegramForwarder.py
+++ MailToTelegramForwarder.py
@@ -1,4 +1,7 @@
 import logging
+import sched
+import threading
+import time
 
 from Forwarder import *
 from MailReader import *
@@ -13,20 +16,27 @@ class MailToTelegramForwarder(Forwarder):
         self.mail = MailReader(self.config)
         self.telegram = Telegram(self.config)
         
-    def start(self):
+    def start(self, scheduler, delay):
         try:
             self.mail.connect()
-            
+                
             # Retrieve all unread mails.
             mails = self.mail.search_mails()
-            
+                
             # Forward the mails to Telegram.
             self.telegram.send_message(mails)
-            
+                
             self.mail.disconnect()
+            
+            scheduler.enterabs(delay, 1, self.start, (scheduler, delay))
         except Exception as error:
             logging.error(error)
 
 if __name__ == '__main__':
     forwarder = MailToTelegramForwarder()
-    forwarder.start()
+    
+    scheduler = sched.scheduler(time.time, time.sleep)
+    scheduler.enterabs(60, 1, forwarder.start, (scheduler, 60))
+    
+    thread = threading.Thread(target=scheduler.run)
+    thread.start()
