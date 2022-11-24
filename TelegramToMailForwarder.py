640701041e6d6f98c712782469c37e96306e9b89
Lim Bernard Kenneth <berlim@gemalto.com>
2022-11-24 12:31:34 +0800

    Telegram to Mail - bot confirmation before sending email

--- TelegramToMailForwarder.py
+++ TelegramToMailForwarder.py
@@ -8,6 +8,10 @@ from email.message import EmailMessage
 class TelegramToMailForwarder(Forwarder):
     mail: MailSender
     telegram: Telegram
+    saved_message = ''
+    saved_reply_to_msg = ''
+    saved_username = ''
+    user_name = ''
     
     def __init__(self):
         super().__init__()
@@ -24,7 +28,29 @@ class TelegramToMailForwarder(Forwarder):
                 if(message.reply_to_message is not None): 
                     reply_to_msg = message.reply_to_message.text
                     if(re.search('From:',reply_to_msg) is not None and re.search('Subject:',reply_to_msg) is not None): 
-                        self.mail.send_mail(reply_to_msg, message.text)
+                        # get the username info from the telegram message
+                        self.user_name = ' '.join(filter(None, (message.from_user.first_name, message.from_user.last_name,)))
+                        if(message.from_user.username is not None): 
+                            self.user_name += '(' + message.from_user.username + ')'
+
+                        # save the information before the bot asks  send email or not
+                        self.saved_username = self.user_name
+                        self.saved_message = message.text + '\n- ' + self.user_name
+                        self.saved_reply_to_msg = reply_to_msg
+
+                        # send a confirmation message
+                        self.telegram.bot.reply_to(message, "Respond to Email (Y/N)?")
+
+                    if(re.search('Respond to Email',reply_to_msg) is not None and (message.text.lower() == 'y' or message.text.lower() == 'yes')): 
+                        # get the username info from the confirmation message
+                        self.user_name = ' '.join(filter(None, (message.from_user.first_name, message.from_user.last_name,)))
+                        if(message.from_user.username is not None): 
+                            self.user_name += '(' + message.from_user.username + ')'
+
+                        # check if the confirmation message is from the same user who initially responded
+                        if(self.saved_username == self.user_name):
+                            # if so, send the email
+                            self.mail.send_mail(self.saved_reply_to_msg, self.saved_message)
 
             self.telegram.bot.infinity_polling();
             
