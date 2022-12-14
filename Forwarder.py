33eee5eaad3cc61e1598853c5ec27f34bbb75b9c
qcoliveros@gmail.com <SSuCM0b5Aj44>
2022-12-14 19:12:35 +0800

    Used log file.

--- Forwarder.py
+++ Forwarder.py
@@ -6,7 +6,7 @@ class Forwarder:
     config: Config
     
     def __init__(self):
-        logging.basicConfig(#filename='tinybots.log', 
+        logging.basicConfig(filename='tinybots.log', 
                             encoding='utf-8', 
                             format='[%(asctime)s %(name)s %(levelname)s %(module)s.%(funcName)s:%(lineno)d] : %(message)s',
                             level=logging.DEBUG)
